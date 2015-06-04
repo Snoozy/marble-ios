ec02b0dfc94ad922e839cce120f216bcf9d17c26
ADaley121 <ajd93@cornell.edu>
2015-06-04 15:48:22 -0400

    Finished Documentation

--- Cillo/Supporting Files/Schemes.swift
+++ Cillo/Supporting Files/Schemes.swift
@@ -15,12 +15,12 @@ struct ColorScheme {
   
   // MARK: Properties
   
-  /// Describes the color scheme that this instance represents
+  /// Describes the color scheme that this instance represents.
   var scheme: ColorSchemeOptions
   
   // MARK: Static Variables
   
-  /// Current scheme used in the live version of the app
+  /// Current scheme used in the live version of the app.
   static var defaultScheme: ColorScheme {
     return ColorScheme(scheme: .Gray)
   }
@@ -34,7 +34,10 @@ struct ColorScheme {
     self.scheme = scheme
   }
   
-  func navigationBarColor() -> UIColor {
+  // MARK: Setup Helper Functions
+  
+  /// :returns: The background color of activity indicator views.
+  func activityIndicatorBackgroundColor() -> UIColor {
     switch scheme {
     case .Blue:
       return UIColor.cilloBlue()
@@ -43,6 +46,50 @@ struct ColorScheme {
     }
   }
   
+  /// :returns: The style of the activity indicator in activity indiciator views.
+  func activityIndicatoryStyle() -> UIActivityIndicatorViewStyle {
+    switch scheme {
+    case .Blue:
+      return .WhiteLarge
+    case .Gray:
+      return .WhiteLarge
+    }
+  }
+  
+  /// :returns: The text color of activity indicator views.
+  func activityIndicatorTextColor() -> UIColor {
+    switch scheme {
+    case .Blue:
+      return UIColor.whiteColor()
+    case .Gray:
+      return UIColor.darkTextColor()
+    }
+  }
+
+  /// :returns: The background color of any bar that appears above the keyboard.
+  /// :returns: For example, the new comment bar.
+  func barAboveKeyboardColor() -> UIColor {
+    switch scheme {
+    case .Blue:
+      return UIColor.cilloBlue()
+    case .Gray:
+      return UIColor.cilloGray()
+    }
+  }
+  
+  /// :returns: The color of the text of the button on any bar that appears above the keyboard.
+  /// :returns: For example, the reply button on the new comment bar.
+  func barAboveKeyboardTouchableTextColor() -> UIColor {
+    switch scheme {
+    case .Blue:
+      return UIColor.whiteColor()
+    case .Gray:
+      return UIColor.darkTextColor()
+    }
+  }
+  
+  /// :returns: The color to be displayed as the color of all bar button items.
+  /// :returns: **Note:** The bar button color is the `tintColor` property.
   func barButtonItemColor() -> UIColor {
     switch scheme {
     case .Blue:
@@ -53,51 +100,7 @@ struct ColorScheme {
     
   }
   
-  func navigationBarTitleColor() -> UIColor {
-    switch scheme {
-    case .Blue:
-      return UIColor.whiteColor()
-    case .Gray:
-      return UIColor.lighterBlack()
-    }
-  }
-  
-  func touchableTextColor() -> UIColor {
-    switch scheme {
-    case .Blue:
-      return UIColor.cilloBlue()
-    case .Gray:
-      return UIColor.grayColor()
-    }
-  }
-  
-  func solidButtonBackgroundColor() -> UIColor {
-    switch scheme {
-    case .Blue:
-      return UIColor.cilloBlue()
-    case .Gray:
-      return UIColor.cilloGray()
-    }
-  }
-  
-  func solidButtonTextColor() -> UIColor {
-    switch scheme {
-    case .Blue:
-      return UIColor.whiteColor()
-    case .Gray:
-      return UIColor.darkTextColor()
-    }
-  }
-  
-  func textFieldBackgroundColor() -> UIColor {
-    switch scheme {
-    case .Blue:
-      return UIColor.cilloBlueWithAlpha(0.1)
-    case .Gray:
-      return UIColor.cilloGrayWithAlpha(0.4)
-    }
-  }
-  
+  /// :returns: The color to be displayed as the background color of the bottom bordered text fields.
   func bottomBorderedTextFieldBackgroundColor() -> UIColor {
     switch scheme {
     case .Blue:
@@ -107,24 +110,7 @@ struct ColorScheme {
     }
   }
   
-  func thinLineBackgroundColor() -> UIColor {
-    switch scheme {
-    case .Blue:
-      return UIColor.defaultTableViewDividerColor()
-    case .Gray:
-      return UIColor.defaultTableViewDividerColor()
-    }
-  }
-  
-  func statusBarStyle() -> UIStatusBarStyle {
-    switch scheme {
-    case .Blue:
-      return .LightContent
-    case .Gray:
-      return .Default
-    }
-  }
-  
+  /// :returns: The color of the custom dividers implemented in most CustomTableViewController classes.
   func dividerBackgroundColor() -> UIColor {
     switch scheme {
     case .Blue:
@@ -134,51 +120,7 @@ struct ColorScheme {
     }
   }
   
-  func segmentedControlSelectedColor() -> UIColor {
-    switch scheme {
-    case .Blue:
-      return UIColor.grayColor()
-    case .Gray:
-      return UIColor.grayColor()
-    }
-  }
-  
-  func segmentedControlUnselectedColor() -> UIColor {
-    switch scheme {
-    case .Blue:
-      return UIColor.whiteColor()
-    case .Gray:
-      return UIColor.whiteColor()
-    }
-  }
-  
-  func barAboveKeyboardColor() -> UIColor {
-    switch scheme {
-    case .Blue:
-      return UIColor.cilloBlue()
-    case .Gray:
-      return UIColor.cilloGray()
-    }
-  }
-  
-  func barAboveKeyboardTouchableTextColor() -> UIColor {
-    switch scheme {
-    case .Blue:
-      return UIColor.whiteColor()
-    case .Gray:
-      return UIColor.darkTextColor()
-    }
-  }
-  
-  func opTextColor() -> UIColor {
-    switch scheme {
-    case .Blue:
-      return UIColor.orangeColor()
-    case .Gray:
-      return UIColor.orangeColor()
-    }
-  }
-  
+  /// :returns: The color of the name in any cell that represents a user that is the end user.
   func meTextColor() -> UIColor {
     switch scheme {
     case .Blue:
@@ -188,7 +130,60 @@ struct ColorScheme {
     }
   }
   
-  func activityIndicatorBackgroundColor() -> UIColor {
+  /// :returns: The color to be displayed as the navigation bar background.
+  /// :returns: **Note:** The background is the `barTint` property.
+  func navigationBarColor() -> UIColor {
+    switch scheme {
+    case .Blue:
+      return UIColor.cilloBlue()
+    case .Gray:
+      return UIColor.cilloGray()
+    }
+  }
+
+  /// :returns: The color to be displayed as the color of the title of the navigation bar.
+  /// :returns: **Note:** The title color is set through the `titleTextAttributes` `NSForegroundColorAttributeName` key.
+  func navigationBarTitleColor() -> UIColor {
+    switch scheme {
+    case .Blue:
+      return UIColor.whiteColor()
+    case .Gray:
+      return UIColor.lighterBlack()
+    }
+  }
+  
+  /// :returns: The color of the name in a comment cell that represents a comment posted by the original poster of the post.
+  func opTextColor() -> UIColor {
+    switch scheme {
+    case .Blue:
+      return UIColor.orangeColor()
+    case .Gray:
+      return UIColor.orangeColor()
+    }
+  }
+  
+  /// :returns: The color of the selected segement of all segmented controls.
+  func segmentedControlSelectedColor() -> UIColor {
+    switch scheme {
+    case .Blue:
+      return UIColor.grayColor()
+    case .Gray:
+      return UIColor.grayColor()
+    }
+  }
+  
+  /// :returns: The color of the unselected segement of all segmented controls.
+  func segmentedControlUnselectedColor() -> UIColor {
+    switch scheme {
+    case .Blue:
+      return UIColor.whiteColor()
+    case .Gray:
+      return UIColor.whiteColor()
+    }
+  }
+  
+  /// :returns: The color to be displayed as the background color of all buttons with a background.
+  func solidButtonBackgroundColor() -> UIColor {
     switch scheme {
     case .Blue:
       return UIColor.cilloBlue()
@@ -197,7 +192,8 @@ struct ColorScheme {
     }
   }
   
-  func activityIndicatorTextColor() -> UIColor {
+  /// :returns: The color to be displayed as the text color of all buttons with a background.
+  func solidButtonTextColor() -> UIColor {
     switch scheme {
     case .Blue:
       return UIColor.whiteColor()
@@ -206,12 +202,43 @@ struct ColorScheme {
     }
   }
   
-  func activityIndicatoryStyle() -> UIActivityIndicatorViewStyle {
+  /// :returns: The status bar style for the application.
+  func statusBarStyle() -> UIStatusBarStyle {
     switch scheme {
     case .Blue:
-      return .WhiteLarge
+      return .LightContent
     case .Gray:
-      return .WhiteLarge
+      return .Default
+    }
+  }
+  
+  /// :returns: The color to be displayed as the background color of text fields and text views.
+  func textFieldBackgroundColor() -> UIColor {
+    switch scheme {
+    case .Blue:
+      return UIColor.cilloBlueWithAlpha(0.1)
+    case .Gray:
+      return UIColor.cilloGrayWithAlpha(0.4)
+    }
+  }
+  
+  /// :returns: The color of all thin lines that are used as divider accents in the application.
+  func thinLineBackgroundColor() -> UIColor {
+    switch scheme {
+    case .Blue:
+      return UIColor.defaultTableViewDividerColor()
+    case .Gray:
+      return UIColor.defaultTableViewDividerColor()
+    }
+  }
+  
+  /// :returns: The color to be displayed as the text color of all button with white backgrounds.
+  func touchableTextColor() -> UIColor {
+    switch scheme {
+    case .Blue:
+      return UIColor.cilloBlue()
+    case .Gray:
+      return UIColor.grayColor()
     }
   }
 }
@@ -252,15 +279,9 @@ struct DividerScheme {
     self.scheme = scheme
   }
   
-  func multiplePostsDividerHeight() -> CGFloat {
-    switch scheme {
-    case .Thick:
-      return 10.0
-    case .Thin:
-      return 1.0
-    }
-  }
-  
+  // MARK: Setup Helper Functions
+
+  /// :returns: The divider height for any MultipleBoardsTableViewController.
   func multipleBoardsDividerHeight() -> CGFloat {
     switch scheme {
     case .Thick:
@@ -270,7 +291,8 @@ struct DividerScheme {
     }
   }
   
-  func singleUserPostDividerHeight() -> CGFloat {
+  /// :returns: The divider height for any MultiplePostsTableViewController.
+  func multiplePostsDividerHeight() -> CGFloat {
     switch scheme {
     case .Thick:
       return 10.0
@@ -279,6 +301,17 @@ struct DividerScheme {
     }
   }
   
+  /// :returns: The divider height for any SingleBoardTableViewController.
+  func singleBoardDividerHeight() -> CGFloat {
+    switch scheme {
+    case .Thick:
+      return 10.0
+    case .Thin:
+      return 1.0
+    }
+  }
+  
+  /// :returns: The divider height between CommentCells for any SingleUserTableViewController.
   func singleUserCommentDividerHeight() -> CGFloat {
     switch scheme {
     case .Thick:
@@ -288,7 +321,8 @@ struct DividerScheme {
     }
   }
   
-  func singleBoardDividerHeight() -> CGFloat {
+  /// :returns: The divider height between PostCells for any SingleUserTableViewController.
+  func singleUserPostDividerHeight() -> CGFloat {
     switch scheme {
     case .Thick:
       return 10.0
--- Cillo/Supporting Files/StoryboardIdentifiers.swift
+++ Cillo/Supporting Files/StoryboardIdentifiers.swift
@@ -8,128 +8,168 @@
 
 import Foundation
 
+/// Has constants for all segue identifiers in the storyboard.
 struct SegueIdentifiers {
   
   // MARK: LogInViewController
   
-  static let loginToTab = "LoginToTab"
-  
+  /// Segue from LogInViewController to RegisterViewController.
   static let loginToRegister = "LoginToRegister"
+  
+  /// Segue from LogInViewController to TabViewController.
+  static let loginToTab = "LoginToTab"
   
   // MARK: SettingsViewController
   
+  /// Segue from SettingsViewController to TabViewController.
   static let settingsToTab = "SettingsToTab"
   
   // MARK: RegisterViewController
   
+  /// Segue from RegisterViewController to LogInViewController.
   static let registerToLogin = "RegisterToLogin"
   
   // MARK: NewBoardViewController
   
+  /// Segue from NewBoardViewController to TabViewController.
   static let newBoardToTab = "NewBoardToTab"
   
   // MARK: NewPostViewController
   
+  /// Segue from NewPostViewController to TabViewController.
   static let newPostToTab = "NewPostToTab"
   
   // MARK: NewRepostViewController
   
+  /// Segue from NewRepostViewController to TabViewController.
   static let newRepostToTab = "NewRepostToTab"
   
   // MARK: TabViewController
   
-  static let tabToNewRepost = "TabToNewRepost"
-  
+  /// Segue from TabViewController to LogInViewController.
   static let tabToLogin = "TabToLogin"
   
+  /// Segue from TabViewController to NewBoardViewController.
+  static let tabToNewBoard = "TabToNewBoard"
+  
+  /// Segue from TabViewController to NewPosttViewController.
   static let tabToNewPost = "TabToNewPost"
   
-  static let tabToSettings = "TabToSettings"
+  /// Segue from TabViewController to NewRepostViewController.
+  static let tabToNewRepost = "TabToNewRepost"
   
-  static let tabToNewBoard = "TabToNewBoard"
+  /// Segue from TabViewController to SettingsViewController.
+  static let tabToSettings = "TabToSettings"
   
   // MARK: HomeTableViewController
   
-  static let homeToPost = "HomeToPost"
-  
+  /// Segue from HomeTableViewController to BoardTableViewController.
   static let homeToBoard = "HomeToBoard"
   
-  static let homeToUser = "HomeToUser"
-  
+  /// Segue from HomeTableViewController to NewPostViewController.
   static let homeToNewPost = "HomeToNewPost"
+  
+  /// Segue from HomeTableViewController to PostTableViewController.
+  static let homeToPost = "HomeToPost"
+  
+  /// Segue from HomeTableViewController to UserTableViewController.
+  static let homeToUser = "HomeToUser"
   
   // MARK: MyBoardsTableViewController
   
+  /// Segue from MyBoardsTableViewController to BoardTableViewController.
   static let myBoardsToBoard = "MyBoardsToBoard"
   
   // MARK: MeTableViewController
   
-  static let meToPost = "MeToPost"
-  
+  /// Segue from MeTableViewController to BoardTableViewController.
   static let meToBoard = "MeToBoard"
   
+  /// Segue from MeTableViewController to BoardsTableViewController.
   static let meToBoards = "MeToBoards"
+  
+  /// Segue from MeTableViewController to PostTableViewController.
+  static let meToPost = "MeToPost"
   
   // MARK: PostTableViewController
   
+  /// Segue from PostTableViewController to BoardTableViewController.
   static let postToBoard = "PostToBoard"
   
+  /// Segue from PostTableViewController to UserTableViewController.
   static let postToUser = "PostToUser"
   
   // MARK: BoardTableViewController
   
+  /// Segue from BoardTableViewController to NewPostTableViewController.
+  static let boardToNewPost = "BoardToNewPost"
+  
+  /// Segue from BoardTableViewController to PostTableViewController.
   static let boardToPost = "BoardToPost"
   
+  /// Segue from BoardTableViewController to UserTableViewController.
   static let boardToUser = "BoardToUser"
-  
-  static let boardToNewPost = "BoardToNewPost"
   
   // MARK: UserTableViewController
   
-  static let userToPost = "UserToPost"
-  
+  /// Segue from UserTableViewController to BoardTableViewController.
   static let userToBoard = "UserToBoard"
   
+  /// Segue from UserTableViewController to BoardsTableViewController.
   static let userToBoards = "UserToBoards"
+  
+  /// Segue from UserTableViewController to PostTableViewController.
+  static let userToPost = "UserToPost"
   
   // MARK: BoardsTableViewController
   
+  /// Segue from BoardsTableViewController to BoardTableViewController.
   static let boardsToBoard = "BoardsToBoard"
 }
 
+/// Has constants for all cell identifiers and view controller identifiers in the storyboard.
 struct StoryboardIdentifiers {
   
   // MARK: BoardTableViewController
   
+  /// Storyboard identifier for BoardTableViewController.
   static let board = "Board"
   
   // MARK: PostTableViewController
   
+  /// Storyboard identifier for PostTableViewController.
   static let post = "Post"
   
   // MARK: PostCell
   
+  /// Reuse identifier for PostCell.
   static let postCell = "Post"
   
   // MARK: RepostCell
   
+  /// Reuse identifier for RepostCell.
   static let repostCell = "Repost"
   
   // MARK: BoardCell
   
+  /// Reuse identifier for BoardCell.
   static let boardCell = "Board"
   
   // MARK: CommentCell
   
+  /// Reuse identifier for CommentCell.
   static let commentCell = "Comment"
   
   // MARK: UserCell
   
+  /// Reuse identifier for UserCell.
   static let userCell = "User"
   
   // MARK: MultipleBoardsTableViewController Single Button Cells
   
+  /// Reuse identifier for "See All" UITableViewCell in MultipleBoardsTableViewController tableView.
   static let seeAllCell = "SeeAll"
   
+  /// Reuse identifier for "New Board" UITableViewCell in MultipleBoardsTableViewController tableView.
   static let newBoardCell = "NewBoard"
 }
